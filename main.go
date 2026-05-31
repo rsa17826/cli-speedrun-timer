85dd5bb657f9b9e4bb6aa026bf9e425390c07c27
nyix <rsa17826@email.vccs.edu>
2026-05-31 14:06:07 -0400

    NO MESSAGE SET

--- main.go
+++ main.go
@@ -4,11 +4,12 @@ import (
 	"fmt"
 	"log"
 	"os/exec"
-	"time"
 
 	"github.com/rsa17826/go-input-lib"
 	"github.com/rsa17826/input-manager/IMan"
 )
+
+var im IMan.ManagerConnection
 
 func main() {
 	level := 0
@@ -19,33 +20,33 @@ func main() {
 		{547, 808},
 		{935, 814},
 	}
-	im, err := IMan.Connect(IMan.ModeListen)
+	im, err := IMan.Connect(IMan.ModeListen, IMan.ModeInjection)
 	print(levelPos[level])
 	if err != nil {
 		panic(err)
 	}
-	ticker := time.NewTicker(1 * time.Millisecond)
-	done := make(chan bool)
-	startTime := time.Now()
-	go func() {
-		for {
-			select {
-			case <-done:
-				return
-			case <-ticker.C:
-				elapsed := time.Since(startTime)
+	// ticker := time.NewTicker(1 * time.Millisecond)
+	// done := make(chan bool)
+	// startTime := time.Now()
+	// go func() {
+	// 	for {
+	// 		select {
+	// 		case <-done:
+	// 			return
+	// 		case <-ticker.C:
+	// 			elapsed := time.Since(startTime)
 
-				// Extract hours, minutes, and seconds from the duration
-				h := int(elapsed.Hours())
-				m := int(elapsed.Minutes()) % 60
-				s := int(elapsed.Seconds()) % 60
-				ms := int(elapsed.Milliseconds()) % 1000
+	// 			// Extract hours, minutes, and seconds from the duration
+	// 			h := int(elapsed.Hours())
+	// 			m := int(elapsed.Minutes()) % 60
+	// 			s := int(elapsed.Seconds()) % 60
+	// 			ms := int(elapsed.Milliseconds()) % 1000
 
-				// Format as HH:MM:SS.mmm (padded with leading zeros)
-				fmt.Printf("\rTimer: %02d:%02d:%02d.%03d", h, m, s, ms)
-			}
-		}
-	}()
+	// 			// Format as HH:MM:SS.mmm (padded with leading zeros)
+	// 			fmt.Printf("\nTimer: %02d:%02d:%02d.%03d", h, m, s, ms)
+	// 		}
+	// 	}
+	// }()
 	for {
 		ev, err := im.ReadNext()
 		if err != nil {
@@ -55,16 +56,60 @@ func main() {
 		switch ev.Event.Code {
 		case input.KEY_W, input.KEY_A, input.KEY_S, input.KEY_D:
 			{
-				// print("start")
+				print("start")
+			}
+		case input.BTN_RIGHT:
+			{
+				print("asdasd")
+				playLevel(1)
 			}
 		}
 	}
 }
+func playLevel(i int) {
+	// playbtn
+	err := im.Send(IMan.WireEvent{
+		Type:  input.EV_REL,
+		Code:  input.REL_X,
+		Value: -9,
+	})
+	if err != nil {
+		log.Printf("Error sending movement: %v", err)
+	}
+
+	err = im.Send(IMan.WireEvent{})
+	if err != nil {
+		log.Printf("Error sending sync: %v", err)
+	}
+	// moveMouse(596, 223)
+	// time.Sleep(10 * time.Millisecond)
+	// im.Send(IMan.WireEvent{
+	// 	// Type: input,
+	// 	Value: 1,
+	// 	Code:  input.BTN_LEFT,
+	// })
+	// im.Send(IMan.WireEvent{})
+	// time.Sleep(10 * time.Millisecond)
+	// im.Send(IMan.WireEvent{
+	// 	Type: input.EV_REL,
+	// 	// Type:  input.EV_ABS,
+	// 	Value: -9,
+	// 	Code:  input.REL_X,
+	// })
+	// im.Send(IMan.WireEvent{})
+	// im.Send(IMan.WireEvent{
+	// 	// Type: input,
+	// 	Value: 0,
+	// 	Code:  input.BTN_LEFT,
+	// })
+	// im.Send(IMan.WireEvent{})
+	// time.Sleep(10 * time.Millisecond)
+}
 func moveMouse(x, y int) {
-	cmd := exec.Command("hyprctl", "dispatch", fmt.Sprintf("hl.dsp.movecursor(%d, %d)", x, y))
+	cmd := exec.Command("hyprctl", "dispatch", fmt.Sprintf("hl.dsp.cursor.move({ x = %d, y = %d})", x, y))
 	output, err := cmd.CombinedOutput()
 	if err != nil {
 		log.Fatalf("Command failed with error: %v\nOutput: %s", err, string(output))
 	}
-	// fmt.Printf("Success:\n%s\n", string(output))
+	fmt.Printf("Success:\n%s\n", string(output))
 }
