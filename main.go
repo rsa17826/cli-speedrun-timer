cd2300803479b2a8d64b97ff0a828a449c7c5e82
nyix <rsa17826@email.vccs.edu>
2026-05-31 12:38:38 -0400

    NO MESSAGE SET

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os/exec"
+	"time"
 
 	"github.com/rsa17826/go-input-lib"
 	"github.com/rsa17826/input-manager/IMan"
@@ -23,15 +24,38 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	ticker := time.NewTicker(10 * time.Millisecond)
+	done := make(chan bool)
+	startTime := time.Now()
+	go func() {
+		for {
+			select {
+			case <-done:
+				return
+			case <-ticker.C:
+				elapsed := time.Since(startTime)
+
+				// Extract hours, minutes, and seconds from the duration
+				h := int(elapsed.Hours())
+				m := int(elapsed.Minutes()) % 60
+				s := int(elapsed.Seconds()) % 60
+				ms := int(elapsed.Milliseconds()) % 1000
+
+				// Format as HH:MM:SS.mmm (padded with leading zeros)
+				fmt.Printf("\rTimer: %02d:%02d:%02d.%03d", h, m, s, ms)
+			}
+		}
+	}()
 	for {
 		ev, err := im.ReadNext()
 		if err != nil {
 			panic(err)
 		}
+
 		switch ev.Event.Code {
 		case input.KEY_W, input.KEY_A, input.KEY_S, input.KEY_D:
 			{
-				print("start")
+				// print("start")
 			}
 		}
 	}
